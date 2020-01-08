ea5999f0518f156bd63a52c920fca5dafead83db
shin <sindepal@gmail.com>
2020-01-08 13:53:31 +0900

    AWS CDK를 이용한 람다 스터디

--- README.md
+++ README.md
@@ -6,3 +6,9 @@
  * `cdk deploy`      deploy this stack to your default AWS account/region
  * `cdk diff`        compare deployed stack with current state
  * `cdk synth`       emits the synthesized CloudFormation template
+
+
+{
+  "app": "npx ts-node bin/aws_lambda.ts"
+}
+
--- lib/aws_lambda-stack.ts
+++ lib/aws_lambda-stack.ts
@@ -1,18 +1,90 @@
-import sns = require('@aws-cdk/aws-sns');
-import subs = require('@aws-cdk/aws-sns-subscriptions');
-import sqs = require('@aws-cdk/aws-sqs');
 import cdk = require('@aws-cdk/core');
+import lambda = require('@aws-cdk/aws-lambda');
+import s3 = require('@aws-cdk/aws-s3');
+import {BucketAccessControl, HttpMethods} from '@aws-cdk/aws-s3';
+import {LambdaDestination} from '@aws-cdk/aws-s3-notifications';
+import {Duration} from "@aws-cdk/core";
 
 export class AwsLambdaStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'AwsLambdaQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    //람다 레이어 작성 방법.
+    //name 속성들은 패턴이 존재한다.  pattern: (arn:[a-zA-Z0-9-]+:lambda:[a-zA-Z0-9-]+:\d{12}:layer:[a-zA-Z0-9-_]+)|[a-zA-Z0-9-_] )
+    // const imageLayers = new lambda.LayerVersion(this, "imageLayers",{
+    //   code : new lambda.AssetCode('lambda_layers/image_layers'),
+    //   compatibleRuntimes : [lambda.Runtime.NODEJS_10_X, lambda.Runtime.NODEJS_8_10],
+    //   layerVersionName: 'imageThumnailLayers',
+    //   description : 'jpg/png/jpeg thumnail modules'
+    // });
+    //
+    // const mp4Layers = new lambda.LayerVersion(this, "mp4Layers",{
+    //   code : new lambda.AssetCode('lambda_layers/image_layers'),
+    //   compatibleRuntimes : [lambda.Runtime.NODEJS_10_X, lambda.Runtime.NODEJS_8_10],
+    //   layerVersionName: 'mp4Thumnail',
+    //   description : 'mp4/gif Thumnail'
+    // });
+
+    const tunmnailLambda = new lambda.Function(this, "s3ImageLambda", {
+      code: new lambda.AssetCode('lambdas/imageThumnail'),
+      runtime: lambda.Runtime.NODEJS_10_X,
+      handler: 's3_image_lambda.handler',
+      // 레이어 등록
+      // layers : [imageLayers]
     });
 
-    const topic = new sns.Topic(this, 'AwsLambdaTopic');
+    const gifLambda = new lambda.Function(this, "gifLambda", {
+      code: new lambda.AssetCode('lambdas/gifToMp4'),
+      runtime: lambda.Runtime.NODEJS_10_X,
+      handler: 'gifToMp4Lambda_test.handler',
+      timeout: Duration.minutes(2)
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const thumnailBucket = new s3.Bucket(this, 'thumBucket', {
+      bucketName: 'lambda-test-resized',
+      versioned: true,
+      // 모든 Get은 public 접근 설정
+      // publicReadAccess:true,
+      accessControl: BucketAccessControl.AUTHENTICATED_READ
+    });
+
+    thumnailBucket.grantWrite(tunmnailLambda);
+    thumnailBucket.grantWrite(gifLambda);
+
+    thumnailBucket.addCorsRule({
+      allowedOrigins: ['*'],
+      allowedMethods: [HttpMethods.GET, HttpMethods.POST,HttpMethods.PUT, HttpMethods.HEAD ],
+      allowedHeaders: ['*']
+    });
+
+    const bucket = new s3.Bucket(this, 'Bucket', {
+      bucketName: 'lambda-test-shin',
+      versioned: true,
+    });
+
+    bucket.grantRead(tunmnailLambda);
+    bucket.grantReadWrite(gifLambda);
+
+    bucket.addCorsRule({
+      allowedOrigins: ['*'],
+      allowedMethods: [HttpMethods.GET, HttpMethods.POST,HttpMethods.PUT, HttpMethods.HEAD ],
+      allowedHeaders: ['*']
+    });
+
+    bucket.addEventNotification(s3.EventType.OBJECT_CREATED,
+        new LambdaDestination(tunmnailLambda),
+        { prefix: 'origin/', suffix : '.jpg' } );
+
+    bucket.addEventNotification(s3.EventType.OBJECT_CREATED,
+        new LambdaDestination(tunmnailLambda),
+        { prefix: 'origin/', suffix : '.png' } );
+
+    bucket.addEventNotification(s3.EventType.OBJECT_CREATED,
+        new LambdaDestination(tunmnailLambda),
+        { prefix: 'origin/', suffix : '.jpeg' } );
+
+    bucket.addEventNotification(s3.EventType.OBJECT_CREATED,
+        new LambdaDestination(gifLambda),
+        { prefix: 'origin/', suffix : '.gif' } );
   }
 }
